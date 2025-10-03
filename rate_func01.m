f10c24ced251e98167373e2b0f1f5b5c4b4aa22d
Anagha <118123726+anaghabbu@users.noreply.github.com>
2025-10-03 16:27:27 -0400

    rate one and two functions

---
+++ rate_func01.m
@@ -0,0 +1,7 @@
+function dXdt = rate_func01(t,X)
+    dXdt = -5*X + 5*cos(t) - sin(t);
+end
+
+function X = solution01(t)
+    X = cos(t);
+end
---
+++ rate_func02.m
@@ -0,0 +1,7 @@
+function dXdt = rate_func02(t,X)
+    dXdt = [0,-1;1,0]*X;
+end
+
+function X = solution02(t)
+    X = [cos(t);sin(t)];
+end
