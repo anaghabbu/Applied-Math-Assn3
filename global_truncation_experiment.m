c86d39c8dab0f3bae6fe1e9f7caef18bf146e57e
Pauline <cocoa4pauline@gmail.com>
2025-10-09 20:02:33 -0400

    plots local and global errors

--- global_truncation_experiment.m
+++ global_truncation_experiment.m
@@ -1,21 +1,18 @@
-% WORK IN PROGRESS. I think my function calls are wrong and 
-% I need to incorporate loglog_fit but not sure how
-% MAYBE [p,k] = loglog_fit(h_step, errs, 1)
 
-% Local: the error for a single time step
+% GLOBAL: the error across the entire integration interval
 % log-log fit to estimate order p
 
-%[p,k]: the regressed values for relationship y = k*x^p
-%function [p,k] = loglog_fit(x_regression,y_regression,varargin)
 
-function local_truncation_experiment()
-    tref = 0.492;
+function global_truncation_experiment()
+    t0 = 0;
+    tf = 1;
+    t_interval = [t0, tf];
     sol = @solution01;
     rate = @rate_func01;
 
     % Single step function methods
-    forEu = @forward_euler_step
-    expMid = @explicit_midpoint_step
+    forEu = @forward_euler_fixed_step_integration
+    expMid = @explicit_midpoint_fixed_step_integration
 
     h_step = logspace(-5, 1, 100) % 1e-5 to 1e1 100 points
     figure; hold on;
@@ -24,14 +21,14 @@ function local_truncation_experiment()
         errs = zeros(size(h_step));
         for i = 1:length(h_step)
             h = h_step(i);
-            XA = sol(tref); % finding exact X at the tref X spot
-            [XB, ~] = forEu(rate, tref, XA, h);  % one step from exact state
-            X_true = sol(tref + h);
+            XA = sol(t_interval); % finding exact X at the tref X spot
+            [XB, ~] = forEu(rate, t_interval, XA, h);  % one step from exact state
+            X_true = sol(t_interval + h);
             errs(i) = norm(XB - X_true);        % norm handles vector/scalar
 
         end
         [p,k] = loglog_fit(h_step(:), errs(:)); % converts to column vectors
-        loglog(h_step, errs 'o-'); hold on;
+        loglog(h_step, errs, 'o-'); hold on;
         disp(p);
         disp(k);
         xlabel('h (step size)');
@@ -41,13 +38,13 @@ function local_truncation_experiment()
         for i = 1:length(h_step)
             h = h_step(i);
             XA = sol(tref); % finding exact X at the tref X spot
-            [XB, ~] = expMid(rate, tref, XA, h);  % one step from exact state
-            X_true = sol(tref + h);
+            [XB, ~] = expMid(rate, t_interval, XA, h);  % one step from exact state
+            X_true = sol(t_interval + h);
             errs(i) = norm(XB - X_true);        % norm handles vector/scalar
            
         end
-        [p,k] = loglog_fit(h, errs);
-        loglog(errs, h, 'o-');
+        [p,k] = loglog_fit(h_step(:), errs(:));
+        loglog(h_step, errs, 'k-');
         disp(p);
         disp(k);
         xlabel('h (step size)');
--- local_truncation_experiment.m
+++ local_truncation_experiment.m
@@ -20,20 +20,41 @@ function local_truncation_experiment()
     h_step = logspace(-5, 1, 100) % 1e-5 to 1e1 100 points
     figure; hold on;
 
-    for i = 1:length(forEu)
+
         errs = zeros(size(h_step));
-        for r = 1:length(h_step)
-            h = h_step(i)
-            XA = sol(tref) % finding exact X at the tref X spot
+        for i = 1:length(h_step)
+            h = h_step(i);
+            XA = sol(tref); % finding exact X at the tref X spot
             [XB, ~] = forEu(rate, tref, XA, h);  % one step from exact state
+            X_true = sol(tref + h);
+            errs(i) = norm(XB - X_true);        % norm handles vector/scalar
+
+        end
+        [p,k] = loglog_fit(h_step(:), errs(:)); % converts to column vectors
+        loglog(h_step, errs, 'o-'); hold on;
+        disp(p);
+        disp(k);
+        xlabel('h (step size)');
+        ylabel('Error');
+        legend('Forward Euler', 'Explicit Midpoint');
+% 
+        for i = 1:length(h_step)
+            h = h_step(i);
+            XA = sol(tref); % finding exact X at the tref X spot
             [XB, ~] = expMid(rate, tref, XA, h);  % one step from exact state
             X_true = sol(tref + h);
             errs(i) = norm(XB - X_true);        % norm handles vector/scalar
-            [p,k] = loglog_fit(h, errs(i), 1);
-            plot(errs(i), h);
-            
+           
         end
+        [p,k] = loglog_fit(h_step(:), errs(:));
+        loglog(h_step, errs, 'o-');
+        disp(p);
+        disp(k);
+        xlabel('h (step size)');
+        ylabel('Error');
+        legend('Forward Euler', 'Explicit Midpoint');
+
         % is y = k*x^p the same as e = O*h^p? 
         % [p,k] = loglog_fit(h_step, errs, 1)
-    end   
+
 end
